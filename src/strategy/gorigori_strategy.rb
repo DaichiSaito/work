a062bc3448be8eabb07019d52d72361859683638
DaichiSaito <hrkedz@gmail.com>
2021-01-14 00:55:00 +0900

    fix 破壊的かどうかの考慮漏れによるバグ

--- src/strategy/gorigori_strategy.rb
+++ src/strategy/gorigori_strategy.rb
@@ -1,7 +1,8 @@
 class GorigoriStrategy
   def detect(original, comparison)
-    original.sort
-    comparison.sort
+    original.sort!
+    comparison.sort!
+
     index = (0...original.size).detect do |idx|
       original[idx] != comparison[idx]
     end
